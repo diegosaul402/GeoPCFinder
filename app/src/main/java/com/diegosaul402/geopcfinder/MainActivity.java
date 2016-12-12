61157cca70983e0afc44715240f25b07215c25c4
Diego Mendoza <diego_saul_402@hotmail.com>
2016-12-12 03:11:34 -0600

    Add menu logic

--- app/src/main/java/com/diegosaul402/geopcfinder/MainActivity.java
+++ app/src/main/java/com/diegosaul402/geopcfinder/MainActivity.java
@@ -1,8 +1,11 @@
 package com.diegosaul402.geopcfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -42,6 +45,29 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentListener = detailsFragment;
         fragmentListener.initList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick(R.id.button)
